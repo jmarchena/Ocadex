5270300566807d0de3d25160bc38793517534548
Jose Marchena <jmt255@gmail.com>
2018-02-13 23:03:35 +0100

    Added OcamonsViewController

--- Ocadex/Ocadex/AppDelegate.swift
+++ Ocadex/Ocadex/AppDelegate.swift
@@ -13,11 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = UIViewController()
+        let ocamonsVC = OcamonsViewController()
+        let navigationController = UINavigationController(rootViewController: ocamonsVC)
+        window.rootViewController = navigationController
         window.makeKeyAndVisible()
         self.window = window
 
---
+++ Ocadex/Ocadex/OcamonsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OcamonsViewController.swift
+//  Ocadex
+//
+//  Created by Jose Marchena on 13/02/2018.
+//  Copyright © 2018 Jose Marchena. All rights reserved.
+//
+
+import UIKit
+
+class OcamonsViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+}
