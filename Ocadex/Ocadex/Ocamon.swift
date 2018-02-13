ae4d026435347aa0cff881502816262a7cc11e13
Jose Marchena <jmt255@gmail.com>
2018-02-13 22:21:24 +0100

    Added Ocamon and OcamonType

---
+++ Ocadex/Ocadex/Ocamon.swift
@@ -0,0 +1,16 @@
+//
+//  Ocamon.swift
+//  Ocadex
+//
+//  Created by Jose Marchena on 13/02/2018.
+//  Copyright © 2018 Jose Marchena. All rights reserved.
+//
+
+import UIKit
+
+struct Ocamon {
+    let name: String
+    let type: OcamonType
+    let info: String
+    let image: UIImage
+}
---
+++ Ocadex/Ocadex/OcamonType.swift
@@ -0,0 +1,13 @@
+//
+//  OcamonType.swift
+//  Ocadex
+//
+//  Created by Jose Marchena on 13/02/2018.
+//  Copyright © 2018 Jose Marchena. All rights reserved.
+//
+
+import Foundation
+
+enum OcamonType {
+    
+}
