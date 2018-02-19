6a4fbce5814ceaa211fae546394911aca57f62b2
Jose Marchena <jmt255@gmail.com>
2018-02-19 17:41:47 +0100

    Added AboutViewController

---
+++ Ocadex/Ocadex/AboutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.swift
+//  Ocadex
+//
+//  Created by Jose Marchena on 19/02/2018.
+//  Copyright © 2018 Jose Marchena. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
