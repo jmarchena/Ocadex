88210460ccf6555178c2477073e58e066f4763e2
Jose Marchena <jmt255@gmail.com>
2018-02-21 08:30:21 +0100

    Moved Coordinator to its own file

--- Ocadex/Ocadex/Application/AppDelegate.swift
+++ Ocadex/Ocadex/Application/AppDelegate.swift
@@ -27,33 +27,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 }
-
-final class Coordinator {
-
-    let window: UIWindow
-    let ocamonStore: Store = Store()
-    let rootViewController: UINavigationController = UINavigationController()
-    lazy var ocamonListViewController: OcamonsViewController = OcamonsViewController(store: ocamonStore)
-
-    init(with window: UIWindow) {
-        self.window = window
-    }
-
-    func start() {
-        rootViewController.pushViewController(ocamonListViewController, animated: true)
-        ocamonListViewController.didSelect = showOcamon
-        ocamonListViewController.didTapAbout = showAbout
-        rootViewController.navigationBar.prefersLargeTitles = true
-        window.rootViewController = rootViewController
-        window.makeKeyAndVisible()
-    }
-
-    private func showOcamon(_ ocamon: Ocamon) {
-        rootViewController.show(OcamonViewController(with: ocamon), sender: self)
-    }
-
-    private func showAbout() {
-        let navController = UINavigationController(rootViewController: AboutViewController())
-        ocamonListViewController.present(navController, animated: true, completion: nil)
-    }
-}
